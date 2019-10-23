12426809c40b6ec38acd0a138589e9a86d835ff6
niowa32 <andres7480311@inbox.ru>
2019-10-23 18:27:39 +0300

    refactor: clean up

--- app/api/account-confirmations/index.js
+++ app/api/account-confirmations/index.js
@@ -8,28 +8,19 @@ const postUsers = require('./users/post');
 
 // constants
 const { ROUTES } = require('constants/routes');
-// const { PERMISSIONS } = require('constants/system');
-// const ValidatorSchemes = require('helpers/validators/schemes');
 
-// middlewares
-// const { isHasPermissions } = require('api/middlewares');
-
-// accept registration
 router.get(
     ROUTES.ACCOUNT_CONFIRMATIONS.USERS.BASE + ROUTES.ACCOUNT_CONFIRMATIONS.USERS.GET_ALL,
-    // isHasPermissions([PERMISSIONS.ACCEPT_REGISTRATION]),
     getUsers.getListUsers,
 );
 
 router.get(
     ROUTES.ACCOUNT_CONFIRMATIONS.USERS.BASE + ROUTES.ACCOUNT_CONFIRMATIONS.USERS.GET,
-    // isHasPermissions([PERMISSIONS.ACCEPT_REGISTRATION]),
     getUsers.getAllUserData,
 );
 
 router.post(
     ROUTES.ACCOUNT_CONFIRMATIONS.USERS.BASE + ROUTES.ACCOUNT_CONFIRMATIONS.USERS.POST,
-    // isHasPermissions([PERMISSIONS.ACCEPT_REGISTRATION]),
     postUsers.confirmAccount,
 );
 
