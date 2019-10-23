0753ac73889713f9f67c2204990582495d425841
niowa32 <andres7480311@inbox.ru>
2019-10-23 08:46:11 +0300

    feat: get conditions and terms

--- app/api/conditions-terms/get.js
+++ app/api/conditions-terms/get.js
@@ -1,10 +1,15 @@
-const { success } = require('api/response');
+const { successString } = require('api/response');
 
-// formatters
+// services
+const S3Service = require('services/aws/s3');
+
+const { AWS_S3_BUCKET_NAME, AWS_S3_CONDITIONS_AND_TERMS_FILE } = process.env;
 
 const getConditionsTerms = async (req, res, next) => {
     try {
-        return success(res, {});
+        const s3Object = await S3Service.getObject(AWS_S3_BUCKET_NAME, AWS_S3_CONDITIONS_AND_TERMS_FILE);
+        const template = s3Object.Body.toString();
+        return successString(res, template);
     } catch (error) {
         next(error);
     }
--- app/api/response.js
+++ app/api/response.js
@@ -8,6 +8,10 @@ const success = (res, data, code = SUCCESS_CODES.OK) => {
     res.status(code).send(result);
 };
 
+const successString = (res, data, code = SUCCESS_CODES.OK) => {
+    return res.status(code).send(data);
+};
+
 const reject = (res, type, error = '', code = ERROR_CODES.BAD_REQUEST) => {
     const result = {
         error: {
@@ -21,5 +25,6 @@ const reject = (res, type, error = '', code = ERROR_CODES.BAD_REQUEST) => {
 
 module.exports = {
     success,
+    successString,
     reject,
 };
