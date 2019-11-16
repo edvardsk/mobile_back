dcbdad1e6d1641e7bd5200151900670f1f5576d7
niowa32 <andres7480311@inbox.ru>
2019-11-16 10:30:44 +0300

    refactor: validation schemes

---
+++ app/helpers/validators/schemes/patterns.js
@@ -0,0 +1,20 @@
+const DIGITS_VALIDATION_PATTERN = '^\\d+$';
+const PASSWORD_VALIDATION_PATTERN = '^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$';
+const URL_VALIDATION_PATTERN = '^(?:http(s)?:\\/\\/)?[\\w.-]+(?:\\.[\\w\\.-]+)+[\\w\\-\\._~:/?#[\\]@!\\$&\'\\(\\)\\*\\+,;=.]+$';
+const LETTERS_AND_DIGITS_VALIDATION_PATTERN = '^[a-zA-Z0-9]*$';
+// const STATE_REGISTRATION_CERTIFICATE_NUMBER_VALIDATION_PATTERN = '^[A-Z]{2}.[0-9]{2}.[0-9]{2}.[0-9]{2}.[0-9]{3}.[A-Z]{1}.[0-9]{6}.[0-9]{2}.[0-9]{2}$';
+const DOUBLE_NUMBER_VALIDATION_PATTERN = '^-?[0-9]+\\.[0-9]+$';
+
+const SUPPORTED_MIMTYPES = ['application/pdf', 'image/jpeg'];
+
+const POSTGRES_MAX_STRING_LENGTH = 255;
+
+module.exports = {
+    DIGITS_VALIDATION_PATTERN,
+    PASSWORD_VALIDATION_PATTERN,
+    URL_VALIDATION_PATTERN,
+    LETTERS_AND_DIGITS_VALIDATION_PATTERN,
+    DOUBLE_NUMBER_VALIDATION_PATTERN,
+    SUPPORTED_MIMTYPES,
+    POSTGRES_MAX_STRING_LENGTH,
+};
---
+++ app/helpers/validators/schemes/sorting-pagination.js
@@ -0,0 +1,211 @@
+// constants
+const {
+    PAGINATION_PARAMS,
+    SORTING_PARAMS,
+    SORTING_DIRECTIONS,
+    COMPANY_EMPLOYEES_SORT_COLUMNS,
+    COMPANIES_SORT_COLUMNS,
+    COMPANIES_ECONOMIC_SETTINGS_SORT_COLUMNS,
+    USERS_SORT_COLUMNS,
+} = require('constants/pagination-sorting');
+const { ARRAY_ROLES_WITHOUT_ADMIN } = require('constants/system');
+const { CARGO_STATUSES } = require('constants/cargo-statuses');
+const { SQL_TABLES, HOMELESS_COLUMNS } = require('constants/tables');
+
+// patterns
+const {
+    DIGITS_VALIDATION_PATTERN,
+} = require('./patterns');
+
+const colsUsers = SQL_TABLES.USERS.COLUMNS;
+const colsCompanies = SQL_TABLES.COMPANIES.COLUMNS;
+
+const basePaginationQuery = {
+    properties: {
+        [PAGINATION_PARAMS.PAGE]: {
+            type: 'string',
+            pattern: DIGITS_VALIDATION_PATTERN,
+        },
+        [PAGINATION_PARAMS.LIMIT]: {
+            type: 'string',
+            pattern: DIGITS_VALIDATION_PATTERN,
+        },
+    },
+};
+
+const basePaginationModifyQuery = {
+    properties: {
+        [PAGINATION_PARAMS.PAGE]: {
+            parse_pagination_options: {},
+        },
+        [PAGINATION_PARAMS.LIMIT]: {
+            parse_pagination_options: {},
+        },
+    },
+};
+
+const baseSortingSortingDirectionQuery = {
+    properties: {
+        [SORTING_PARAMS.SORT_DIRECTION]: {
+            type: 'string',
+            enum: [SORTING_DIRECTIONS.ASC, SORTING_DIRECTIONS.DESC],
+        },
+    },
+};
+
+const companyEmployeesSortColumnQuery = {
+    properties: {
+        [SORTING_PARAMS.SORT_COLUMN]: {
+            type: 'string',
+            enum: COMPANY_EMPLOYEES_SORT_COLUMNS,
+        }
+    }
+};
+
+const companiesSortColumnQuery = {
+    properties: {
+        [SORTING_PARAMS.SORT_COLUMN]: {
+            type: 'string',
+            enum: COMPANIES_SORT_COLUMNS,
+        }
+    }
+};
+
+const companiesEconomicSettingsSortColumnQuery = {
+    properties: {
+        [SORTING_PARAMS.SORT_COLUMN]: {
+            type: 'string',
+            enum: COMPANIES_ECONOMIC_SETTINGS_SORT_COLUMNS,
+        }
+    }
+};
+
+const usersSortColumnQuery = {
+    properties: {
+        [SORTING_PARAMS.SORT_COLUMN]: {
+            type: 'string',
+            enum: USERS_SORT_COLUMNS,
+        }
+    }
+};
+
+const modifyFilterQuery = {
+    properties: {
+        [HOMELESS_COLUMNS.FILTER]: {
+            parse_string_to_json: {},
+        },
+    },
+};
+
+const companyEmployeesFilterQuery = {
+    properties: {
+        [HOMELESS_COLUMNS.FILTER]: {
+            type: 'object',
+            properties: {
+                [colsUsers.FULL_NAME]: {
+                    type: 'string',
+                },
+            },
+            additionalProperties: false,
+        },
+    },
+};
+
+const usersFilterQuery = {
+    properties: {
+        [HOMELESS_COLUMNS.FILTER]: {
+            type: 'object',
+            properties: {
+                [HOMELESS_COLUMNS.ROLE]: {
+                    type: 'array',
+                    minItems: 1,
+                    uniqueItems: true,
+                    items: {
+                        enum: ARRAY_ROLES_WITHOUT_ADMIN,
+
+                    },
+                },
+            },
+            additionalProperties: false,
+        },
+    },
+};
+
+const companyDriversFilterQuery = {
+    properties: {
+        [HOMELESS_COLUMNS.FILTER]: {
+            type: 'object',
+            properties: {
+                [colsUsers.FULL_NAME]: {
+                    type: 'string',
+                },
+            },
+            additionalProperties: false,
+        },
+    },
+};
+
+const companiesFilterQuery = {
+    properties: {
+        [HOMELESS_COLUMNS.FILTER]: {
+            type: 'object',
+            properties: {
+                [colsCompanies.PRIMARY_CONFIRMED]: {
+                    type: 'boolean',
+                },
+                [colsCompanies.EDITING_CONFIRMED]: {
+                    type: 'boolean',
+                },
+            },
+            additionalProperties: false,
+        },
+    },
+};
+
+const companiesEconomicSettingsFilterQuery = {
+    properties: {
+        [HOMELESS_COLUMNS.FILTER]: {
+            type: 'object',
+            properties: {
+            },
+            additionalProperties: false,
+        },
+    },
+};
+
+const cargosFilterQuery = {
+    properties: {
+        [HOMELESS_COLUMNS.FILTER]: {
+            type: 'object',
+            properties: {
+                [HOMELESS_COLUMNS.STATUS]: {
+                    type: 'array',
+                    minItems: 1,
+                    uniqueItems: true,
+                    items: {
+                        enum: CARGO_STATUSES,
+                    },
+
+                },
+            },
+            additionalProperties: false,
+        },
+    },
+};
+
+module.exports = {
+    basePaginationQuery,
+    basePaginationModifyQuery,
+    baseSortingSortingDirectionQuery,
+    companyEmployeesSortColumnQuery,
+    companiesSortColumnQuery,
+    companiesEconomicSettingsSortColumnQuery,
+    usersSortColumnQuery,
+    modifyFilterQuery,
+    companyEmployeesFilterQuery,
+    usersFilterQuery,
+    companyDriversFilterQuery,
+    companiesFilterQuery,
+    companiesEconomicSettingsFilterQuery,
+    cargosFilterQuery,
+};
