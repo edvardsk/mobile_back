e8bfb8a73b8e53b5453965699d16dd2477711980
niowa32 <andres7480311@inbox.ru>
2019-11-27 09:45:34 +0300

    change: date of updating exchange rates

--- app/cron.js
+++ app/cron.js
@@ -2,7 +2,7 @@ const CronJob = require('cron').CronJob;
 const CronService = require('./services/cron');
 
 new CronJob({
-    cronTime: '0 0 0 * * *', //every day at 00:00:00 by UTC+0
+    cronTime: '0 0 2 * * *', //every day at 00:02:00 by UTC+0
     onTick: CronService.updateExchangeRates,
     start: true,
     timeZone: 'Europe/London',
