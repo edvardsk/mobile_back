59998a39350710c5e817c20fd92f2938b7784a7b
link-alex <manzhula.alex@gmail.com>
2019-12-16 11:00:21 +0300

    [BACK-79] add constraint for cargo distance

--- migrations/20191216100509_add_distance_to_cargo.js
+++ migrations/20191216100509_add_distance_to_cargo.js
@@ -1,12 +1,22 @@
 
 exports.up = function(knex) {
     return knex.schema.alterTable('cargos', function(table) {
-        table.integer('distance');
-    });
+        table.integer('distance').notNull().defaultTo(0);
+    })
+        .then(function () {
+            return Promise.all([
+                knex.raw('ALTER TABLE cargos ADD CONSTRAINT distance_check CHECK (distance >= 0)'),
+            ]);
+        });
 };
 
 exports.down = function(knex) {
-    return knex.schema.alterTable('cargos', function (table) {
-        table.dropColumn('distance');
-    });
+    return Promise.all([
+        knex.raw('ALTER TABLE cargos DROP CONSTRAINT distance_check;'),
+    ])
+        .then(function () {
+            return knex.schema.alterTable('cargos', function (table) {
+                table.dropColumn('distance');
+            });
+        });
 };
