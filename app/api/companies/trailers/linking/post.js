25d3b2bbd6008d802618e5d744144869ddb457a2
Eugene Bezrukov <bezrukov.bsuir@gmail.com>
2019-12-24 12:31:52 +0300

    Transaction calls moved to middleware

--- app/api/companies/trailers/linking/post.js
+++ app/api/companies/trailers/linking/post.js
@@ -2,6 +2,8 @@ const { success } = require('api/response');
 
 // services
 const TrailersServices = require('services/tables/trailers');
+const CarPointsService = require('services/tables/car-points');
+const TableService = require('services/tables');
 
 // constants
 const { SUCCESS_CODES } = require('constants/http-codes');
@@ -10,8 +12,11 @@ const linkTrailerWithCar = async (req, res, next) => {
     try {
         const { trailerId } = req.params;
         const carId = req.body.car_id;
+        const carPointstransactions = await CarPointsService.addPointOnLinking(carId, trailerId);
 
-        await TrailersServices.linkTrailerAndCar(trailerId, carId);
+        const linkTrailerTransaction = await TrailersServices.linkTrailerAndCar(trailerId, carId);
+
+        await TableService.runTransaction([... carPointstransactions, linkTrailerTransaction]);
 
         return success(res, {}, SUCCESS_CODES.NOT_CONTENT);
     } catch (error) {
@@ -22,8 +27,11 @@ const linkTrailerWithCar = async (req, res, next) => {
 const unlinkTrailerFromCar = async (req, res, next) => {
     try {
         const { trailerId } = req.params;
+        const carPointstransactions = await CarPointsService.addPointOnUnlinking(trailerId);
 
-        await TrailersServices.unlinkTrailerFromCar(trailerId);
+        const unlinkTrailerTransaction = await TrailersServices.unlinkTrailerFromCar(trailerId);
+
+        await TableService.runTransaction([... carPointstransactions, unlinkTrailerTransaction]);
 
         return success(res, {}, SUCCESS_CODES.NOT_CONTENT);
     } catch (error) {
--- app/services/tables/trailers.js
+++ app/services/tables/trailers.js
@@ -22,8 +22,6 @@ const {
 
 // services
 const DangerClassesService = require('./danger-classes');
-const CarPointsService = require('./car-points');
-const TablesService = require('./index');
 const DraftTrailersService = require('./draft-trailers');
 
 // constants
@@ -83,19 +81,13 @@ const markAsDeletedAsTransaction = id => [updateRecord(id, {
     [colsTrailers.DELETED]: true,
 }), OPERATIONS.ONE];
 
-const linkTrailerAndCar = async (trailerId, carId) => {
-    await TablesService.runTransaction(await CarPointsService.addPointOnLinking(carId, trailerId));
-    return editRecord(trailerId, {
-        [colsTrailers.CAR_ID]: carId,
-    });
-};
+const linkTrailerAndCar = async (trailerId, carId) => editRecordAsTransaction(trailerId, {
+    [colsTrailers.CAR_ID]: carId,
+});
 
-const unlinkTrailerFromCar = async id => {
-    await TablesService.runTransaction(await CarPointsService.addPointOnUninking(id));
-    return editRecord(id, {
-        [colsTrailers.CAR_ID]: null,
-    });
-};
+const unlinkTrailerFromCar = async id => editRecordAsTransaction(id, {
+    [colsTrailers.CAR_ID]: null,
+});
 
 const getAvailableTrailersPaginationSorting = (companyId, cargoDates, limit, offset, sortColumn, asc, filter) => (
     manyOrNone(selectAvailableTrailersByCompanyIdPaginationSorting(companyId, cargoDates, limit, offset, sortColumn, asc, filter))
